220654127b65a79045601eeb2192132ad4e8dbf1
Makktu <johnmcna@gmail.com>
2021-09-15 23:15:42 +0100

    messing

--- script.js
+++ script.js
@@ -2,26 +2,9 @@
 
 function wheresMySixty() {
     console.log("Starting...");
-    fetch(
-        "https://data.bus-data.dft.gov.uk/api/v1/datafeed/1696/?api_key=93b0e2fee16e881a1ccd4a49736d71c44b376744"
-    )
+    fetch("/response_1631723272107.xml")
         .then((response) => response.text())
         .then((data) => console.log(data));
-    // let newRequest = fetch(
-    //     "https://data.bus-data.dft.gov.uk/api/v1/datafeed/1696/?api_key=93b0e2fee16e881a1ccd4a49736d71c44b376744",
-    //     { mode: "no-cors" }
-    // );
-    // console.log(newRequest);
-    // let request = new XMLHttpRequest();
-    // request.open(
-    //     "GET",
-    //     "https://data.bus-data.dft.gov.uk/api/v1/datafeed/1696/?api_key=93b0e2fee16e881a1ccd4a49736d71c44b376744",
-    //     { mode: "no-cors" }
-    // );
-    // request.send();
-    // request.onload = function () {
-    //     console.log(request.status);
-    // };
 }
 
 const messageArea = document.querySelector(".message-area");
