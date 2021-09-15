ce5ac3a328c58ec0e7ea2b0225bb4b48d360f3ae
Makktu <johnmcna@gmail.com>
2021-09-15 14:25:34 +0100

    Some basic setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# WHERE'S MY 60?
+
+# The most simple bus-tracking webapp possible!
+
+This one-page webapp will be designed to perform just one task: to track the current location of the Stagecoach no. 60 bus servive from Warwick University to Arena Park.
--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 "use strict";
+
+function wheresMySixty() {
+    messageArea.textContent = "Pressed OK";
+    setTimeout(function () {
+        messageArea.textContent = "";
+    }),
+        50000;
+}
+
+const messageArea = document.querySelector(".message-area");
+
+const getButton = document.querySelector(".get-btn");
+
+getButton.addEventListener("click", wheresMySixty);
