f7168bc58832b9cf79253235192439f9e450fb8c
Makktu <johnmcna@gmail.com>
2021-09-16 20:18:43 +0100

    progress, but stuck again

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 "use strict";
 
+function parseData(data) {
+    const jsonData = xmlToJson.parse(data);
+    const allBuses =
+        jsonData.Siri.ServiceDelivery.VehicleMonitoringDelivery.VehicleActivity;
+    console.log(allBuses);
+
+    let location = "";
+
+    // get times of recording
+    for (let bus in allBuses) {
+        let lat =
+            allBuses[bus].MonitoredVehicleJourney.VehicleLocation.Latitude;
+        let lon =
+            allBuses[bus].MonitoredVehicleJourney.VehicleLocation.Longitude;
+        let time = allBuses[bus].RecordedAtTime;
+        console.log(lat, lon, time);
+        fetch(
+            `http://api.positionstack.com/v1/reverse?access_key=0cfcfb7d42c2c2f3e7b21223952129ef&query=${lat},${lon}`
+        )
+            .then((response) => response.text())
+            .then((addr) => (location += addr.street + "///"));
+        // location += addr.street + "///";
+    }
+    console.log(location);
+}
+
 function wheresMySixty() {
     console.log("Starting...");
     const url =
-        "https://cors-anywhere.herokuapp.com/https://data.bus-data.dft.gov.uk/api/v1/datafeed?boundingBox=-1.42625%2C%2052.36964%2C%20-1.59502%2C%2052.45649&operatorRef=SCNH&lineRef=60&api_key=93b0e2fee16e881a1ccd4a49736d71c44b376744";
+        "https://cors.bridged.cc/https://data.bus-data.dft.gov.uk/api/v1/datafeed?boundingBox=-1.42625%2C%2052.36964%2C%20-1.59502%2C%2052.45649&operatorRef=SCNH&lineRef=60&api_key=93b0e2fee16e881a1ccd4a49736d71c44b376744";
     fetch(url)
         .then((response) => response.text())
-        .then((data) => console.log(data));
+        .then((data) => parseData(data));
 }
 
 const messageArea = document.querySelector(".message-area");
@@ -26,3 +52,5 @@ getButton.addEventListener("click", wheresMySixty);
 // OR EVEN:
 
 // https://data.bus-data.dft.gov.uk/api/v1/datafeed?boundingBox=-1.42625%2C%2052.36964%2C%20-1.59502%2C%2052.45649&operatorRef=SCNH&lineRef=60
+
+// positionstack API : 0cfcfb7d42c2c2f3e7b21223952129ef
