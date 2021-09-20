6afbc652ea18be2340d4b1271440427101b56909
Makktu <johnmcna@gmail.com>
2021-09-20 17:35:13 +0100

    little porgress

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 "use strict";
 
+function displayMap() {
+    let lat = 52.38857572521704; // 52.38937773625854
+    let lon = -1.462076773668885; // -1.4575123786926272
+    let time = "15:14:32";
+    messageArea.innerHTML = `<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-1.464309096336365%2C52.38675529520704%2C-1.4572280645370486%2C52.389665835140555&amp;layer=mapnik&amp;marker=${lat}%2C${lon}" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=52.38792&amp;mlon=-1.46105#map=18/52.38792/-1.46105">View Larger Map</a></small>`;
+
+    // lon-0.01 / lat + 0.01
+
+    // 52.38857572521704, -1.462076773668885
+
+    //<iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=-1.4639979600906374%2C52.38671928057508%2C-1.4569169282913208%2C52.38962982288314&amp;layer=mapnik&amp;marker=52.38817621269286%2C-1.460457444190979" style="border: 1px solid black"></iframe><br/><small><a href="https://www.openstreetmap.org/?mlat=52.38818&amp;mlon=-1.46046#map=18/52.38817/-1.46046">View Larger Map</a></small>
+}
+
 function parseData(data) {
+    displayMap();
+    return;
     console.log(data);
     const jsonData = xmlToJson.parse(data);
     const allBuses =
         jsonData.Siri.ServiceDelivery.VehicleMonitoringDelivery.VehicleActivity;
     console.log(jsonData);
+    console.log(allBuses);
 
     let location = "";
 
     // get locations and times of recording
     for (let bus in allBuses) {
-        // if (allBuses[bus].)
+        // console.log(`-----${bus}: `, allBuses[bus]);
         console.log(allBuses[bus].MonitoredVehicleJourney.DirectionRef);
         console.log(allBuses[bus].MonitoredVehicleJourney.Bearing);
         console.log(allBuses[bus].MonitoredVehicleJourney.DestinationRef);
+
         let lat =
             allBuses[bus].MonitoredVehicleJourney.VehicleLocation.Latitude;
         let lon =
@@ -22,7 +39,6 @@ function parseData(data) {
         let time = allBuses[bus].RecordedAtTime;
         console.log(lat, lon, time);
         // window.open(`http://www.google.com/maps/place/${lat},${lon}`);
-        // messageArea.innerHTML = `<img src="https://maps.google.com/maps/api/staticmap?center=${lat},${lon}&zoom=8&size=400x300&sensor=false" style="width: 400px; height: 400px;" />`;
 
         fetch(
             `http://api.positionstack.com/v1/reverse?access_key=0cfcfb7d42c2c2f3e7b21223952129ef&query=${lat},${lon}&output=json&limit=1`
@@ -33,7 +49,12 @@ function parseData(data) {
         // location += addr.street + "///";
     }
     console.log(location);
-    messageArea.textContent = location;
+    //     // messageArea.textContent = location;
+
+    // let lat = 53;
+    // let lon = -1;
+
+    // updateMap(lat, lon);
 }
 
 function wheresMySixty() {
