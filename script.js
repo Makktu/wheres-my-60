0730f5335eeb6b81aa3953fa6f78171aabb6f6bf
Makktu <johnmcna@gmail.com>
2021-09-25 02:40:06 +0100

    adjusted spinner location

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ function wheresMySixty() {
     infoLine.textContent = "";
     messageArea.style = "color: white; font-size: 2.2rem;";
     messageArea.innerHTML =
-        '<br><br><br><i class="fas fa-spinner fa-spin fa-3x fa-fw"></i>';
+        '<br><br><i class="fas fa-spinner fa-spin fa-3x fa-fw"></i>';
     const url =
         "https://cors.bridged.cc/https://data.bus-data.dft.gov.uk/api/v1/datafeed?boundingBox=-1.42625%2C%2052.36964%2C%20-1.59502%2C%2052.45649&operatorRef=SCNH&lineRef=60&api_key=93b0e2fee16e881a1ccd4a49736d71c44b376744";
     fetch(url)
