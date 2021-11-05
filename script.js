4286cf8a929b9bbb647d7a052a3b6e50a6c0cabb
John McNamara <johnmcna@gmail.com>
2021-11-05 15:46:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -72,11 +72,10 @@ function displayMap(lat, lon, time) {
         assessTime() - theHour > 1 ||
         theHour > assessTime() ||
         (lat > 52.4072268987048 && travellingDirection === "INBOUND")
-    )             wheresMySixty();
-
-    
-   
+    )  {
+        wheresMySixty();  
         return;
+    }
     }
 
     theTime = theHour.toString() + theTime.substring(2);
