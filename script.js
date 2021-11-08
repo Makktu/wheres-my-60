e53858bd5daf55dc52847f882fa8ef08540bc379
John McNamara <johnmcna@gmail.com>
2021-11-08 13:23:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -11,10 +11,12 @@ function loadCSS() {
             );
         toggleDark.innerHTML =
             "<i id='toggle-colors' class='fas fa-toggle-on'></i>";
+        isDark = true;
     } else {
         darkMode.parentNode.removeChild(darkMode);
         toggleDark.innerHTML =
             "<i id='toggle-colors' class='fas fa-toggle-off'></i>";
+        isDark = false;
     }
 }
 
@@ -72,10 +74,12 @@ function displayMap(lat, lon, time) {
         assessTime() - theHour > 1 ||
         theHour > assessTime() ||
         (lat > 52.4072268987048 && travellingDirection === "INBOUND")
-    )  {
-        wheresMySixty();  
+    ) {
+        if (!skipToNext) {
+            skipToNext = true;
+            wheresMySixty();
+        }
         return;
-    }
     }
 
     theTime = theHour.toString() + theTime.substring(2);
@@ -86,11 +90,37 @@ function displayMap(lat, lon, time) {
     } is here:`;
 
     messageArea.innerHTML = "";
-    messageArea.innerHTML = `<iframe width="340" height="420" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=${
-        lon - 0.001
-    }%2C${lat - 0.001}%2C${lon + 0.001}%2C${
-        lat + 0.001
-    }&amp;layer=mapnik&amp;marker=${lat}%2C${lon}" style="border: 1px solid black"></iframe>`;
+
+    // ********************************
+
+    if (isDark) {
+        const coords = [lat, lon];
+
+        const map = L.map("map").setView(coords, 18);
+
+        L.tileLayer(
+            "https://{s}.tile.jawg.io/jawg-matrix/{z}/{x}/{y}{r}.png?access-token={accessToken}",
+            {
+                attribution:
+                    '<a href="http://jawg.io" title="Tiles Courtesy of Jawg Maps" target="_blank">&copy; <b>Jawg</b>Maps</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                minZoom: 0,
+                maxZoom: 22,
+                subdomains: "abcd",
+                accessToken:
+                    "VSMYJoLANC0QI2V4TS4B8wGyqgqaw2UHcP93FWce2KjM0Hl9Ujc55dHX9lES5dzc",
+            }
+        ).addTo(map);
+
+        L.marker(coords).addTo(map).bindPopup(`${locationOfBus}`).openPopup();
+    } else {
+        // ********************************
+
+        messageArea.innerHTML = `<iframe width="340" height="420" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.openstreetmap.org/export/embed.html?bbox=${
+            lon - 0.001
+        }%2C${lat - 0.001}%2C${lon + 0.001}%2C${
+            lat + 0.001
+        }&amp;layer=mapnik&amp;marker=${lat}%2C${lon}" style="border: 1px solid black"></iframe>`;
+    }
 }
 
 function parseData(data) {
@@ -118,17 +148,13 @@ function parseData(data) {
             let lon =
                 allBuses[bus].MonitoredVehicleJourney.VehicleLocation.Longitude;
             let time = allBuses[bus].RecordedAtTime;
-            console.log("HERE");            
-            displayMap(lat, lon, time);
-
-//             if (!skipToNext) {
-//                 displayMap(lat, lon, time);
-//             } else {
-//                 skipToNext = false;
-//                 continue;
-//             }
+            if (!skipToNext) {
+                displayMap(lat, lon, time);
+            } else {
+                skipToNext = false;
+                continue;
+            }
             return;
-        }
         }
     }
 }
@@ -138,7 +164,6 @@ function wheresMySixty() {
     messageArea.style = "font-size: 2.5rem;";
     messageArea.innerHTML =
         '<br><br><i class="fas fa-spinner fa-spin fa-3x fa-fw"></i>';
-
     const url =
         "https://api.codetabs.com/v1/proxy?quest=https://data.bus-data.dft.gov.uk/api/v1/datafeed?boundingBox=-1.42625%2C%2052.36964%2C%20-1.59502%2C%2052.45649&operatorRef=SCNH&lineRef=60&api_key=93b0e2fee16e881a1ccd4a49736d71c44b376744";
     fetch(url)
@@ -175,6 +200,19 @@ const infoLine = document.getElementById("info");
 
 const mapPic = document.querySelector(".map-pic");
 
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const userLat = position.coords.latitude;
+            const userLon = position.coords.longitude;
+            console.log(userLat, userLon);
+        },
+        function () {
+            alert("Could not get your position");
+        }
+    );
+}
+
 mapPic.addEventListener("click", () => {
     infoLine.textContent = "Not that one...";
     setTimeout(function () {
@@ -193,3 +231,38 @@ getButtonHome.addEventListener("click", () => {
     travellingDirection = "OUTBOUND";
     wheresMySixty();
 });
+
+// const LeafletMapContainer = document.getElementById("map");
+
+let isDark = true;
+loadCSS();
+
+const apiToken = config.MY_API_TOKEN;
+const key = config.SECRET_API_KEY;
+
+// if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(
+//       function (position) {
+//         const { latitude } = position.coords;
+//         const { longitude } = position.coords;
+//         console.log(`https://www.google.com/maps/@${latitude},${longitude},14z`);
+
+//         const coords = [latitude, longitude];
+
+//         const map = L.map("map").setView(coords, 13);
+
+//         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//           attribution:
+//             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//         }).addTo(map);
+
+//         L.marker(coords)
+//           .addTo(map)
+//           .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+//           .openPopup();
+//       },
+//       function () {
+//         alert("Could not get your position");
+//       }
+//     );
+//   }
